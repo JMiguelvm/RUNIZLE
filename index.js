e38bd4995c1d01410c83b6e3458107000b752e32
L0G4Ntf <mylogan360@gmail.com>
2022-10-15 15:08:38 -0500

    Se agrego un nav menu responsive y publicaciones.

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const searchButton = document.querySelector (".search__button")
 const searchToggle = document.querySelector (".search")
 const lupaAToggle = document.querySelector (".lupa__a")
 const lupaBToggle = document.querySelector (".lupa__b")
+const navToggle = document.querySelector ("#nav__btn")
+const navCToggle = document.querySelector (".nav__bar")
+const navBToggle = document.querySelector (".nav__bar_box")
+const listBUser = document.querySelector (".list__btn-user")
+const listShow = document.querySelector (".list__show")
 
 usuarioItemMain.addEventListener("click", () => {
     usuarioContainer.classList.toggle("usuario__container_visible")
@@ -14,6 +19,16 @@ usuarioItemMain.addEventListener("click", () => {
 usuarioItemMain.addEventListener("click", () => {
     usuario.classList.toggle("usuario_visible")
 })
+
+navToggle.addEventListener("click", () => {
+    navCToggle.classList.toggle("nav__bar_visible")
+    navBToggle.classList.toggle("nav__toggle_visible")
+})
+
+listBUser.addEventListener("click", () => {
+    listShow.classList.toggle("list__show_active")
+})
+
 
 searchButton.addEventListener("click", () => {
     searchToggle.classList.toggle("search__toggle")
